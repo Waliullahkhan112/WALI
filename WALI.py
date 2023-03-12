9710a98572da03a47574bc1cbf1ee34432a39c0a
Waliullahkhan112 <127169355+Waliullahkhan112@users.noreply.github.com>
2023-03-12 16:35:19 +0500

    Update WALI.py

--- WALI.py
+++ WALI.py
@@ -1,10 +1,16 @@
-import os,time
-os.system('clear')
+import os
+os.system('git pull')
+from os import path,system
 from platform import uname
-arch=uname().machine.lower()
-if 'aarch' in arch:
-    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    os.system('chmod 777 dump && ./dump')
-else:
-    print(' \033[1;31m Sorry system or device not support this tools');exit()
-
+bt=uname().machine.lower()
+if 'aarch' in bt:
+    if path.isfile("XD.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/Waliullahkhan112/files/main/XD -o XD")
+    if path.isfile("dump.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/Waliullahkhan112/files/main/dump.so -o dump.so")
+else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+os.system('chmod 777 XD && ./XD')
